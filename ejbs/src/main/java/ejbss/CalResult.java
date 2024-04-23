632991ce13724399b4e3d9da20c18370275f4d3a
Mayarsalahe <150254332+Mayarsalahe@users.noreply.github.com>
2024-04-23 19:17:09 +0200

    commit

---
+++ ejbs/src/main/java/ejbss/CalResult.java
@@ -0,0 +1,14 @@
+package ejbss;
+
+public class CalResult {
+	public CalResult(double result2){
+		Result =result2;
+	}
+	private double Result;
+	public double getResult() {
+		return Result;
+	}
+	public void setResult(int r) {
+		Result =r;
+	}
+}
